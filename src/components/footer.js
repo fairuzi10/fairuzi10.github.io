9e489f46482008bc8b1e8ef1f5b99a42ccd1b783
M Fairuzi Teguh <mfairuzit@gmail.com>
2018-01-31 10:23:32 +0700

    Add privacy policy

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'gatsby-link'
 
 const Footer = styled.footer`
   margin-top: 3em;
   border-top: 1px solid #e5e5e5;
-  padding: 2em 0em;
+  padding: 1.5em 0em;
   text-align: center;
   color: #777;
   background-color: #f8f9fa;
+  font-size: 0.8em;
 `
 
 export default () => (
   <Footer>
-    © Copyright 2018 Muhammad Fairuzi Teguh
+    Muhammad Fairuzi Teguh © 2018<br />
+    <Link to='/privacy-policy/' className='grey-link'>Kebijakan Privasi</Link>
   </Footer>
 )
---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Reg from '../components/reg'
+
+export default () => (
+    <Reg>
+        Last updated: 31 January 2018
+        <hr />
+        <h1>Selamat Datang di Website Fairuzi10!</h1>
+        <div className="text-justify">
+            Terkadang, terutama pada bagian proyek, kami membutuhkan data Anda contohnya untuk 
+            mengasosiasikan data Anda dengan profil Anda. Hal ini tentu membutuhkan autentikasi
+            dan kami memutuskan untuk menggunakan akun Facebook karena cukup mudah dilakukan.
+            Tenang saja, data Anda yang akan kami perlihatkan kepada publik hanyalah nama Anda.
+        </div>
+    </Reg>
+)
