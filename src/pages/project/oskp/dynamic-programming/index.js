a4a9406244f7eb247bdebc84acdf69d031f5cf25
M Fairuzi Teguh <mfairuzit@gmail.com>
2018-03-10 19:41:13 +0700

    Change pdf view for OSKP

--- src/pages/project/oskp/dynamic-programming/index.js
+++ src/pages/project/oskp/dynamic-programming/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Reg from '../../../../components/reg'
 import Comment from 'react-disqus-comments'
 import Helmet from 'react-helmet'
-import file from './dynamic-programming.pdf'
 
 export default ({ data }) => (
   <Reg>
@@ -21,7 +20,7 @@ export default ({ data }) => (
       slide <a href="https://training.ia-toki.org/training/curriculums/1/courses/11/chapters/55/lessons/23/">dynamic programming di TLX</a> terlebih dahulu.
     </div>
     <br />
-    <iframe frameBorder="0" src={ file + "#zoom=150"} scrolling="auto" width="100%" height="600" marginwidth="50"></iframe>
+    <iframe frameBorder="0" src="https://drive.google.com/file/d/1VRDV6y-IBvsks6FVN544nQ1y4mDghgiu/preview" width="100%" height="600"></iframe>
     <hr />
     <Comment
       identifier='dynamic-programming'
