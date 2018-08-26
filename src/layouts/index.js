e28c5075301404f1480ab4f789db8cc39f865164
Muhammad Fairuzi Teguh <mfairuzit@gmail.com>
2018-08-26 22:28:10 +0700

    Remove unused CDN import

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,11 +17,6 @@ const TemplateWrapper = ({ children }) => (
   <FullPage>
     <Helmet
       title='Fairuzi10'
-      link={[
-        { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css'},
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Arvo'},
-        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0-alpha2/katex.min.css'}
-      ]}
       meta={[
         { name: 'google-site-verification', content: 'uu3IRN8Av5aq9r0lEKhIJjoLFIi1T8TFKymt0f-68bk' },
       ]}
