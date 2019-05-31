dfc8ae606864b4dc7907a20561031d712b9e33a8
M. Fairuzi Teguh <mfairuzit@gmail.com>
2019-05-31 11:59:34 +0900

    Fix Vue global

--- src/pages/project/jadwal-siak/index.js
+++ src/pages/project/jadwal-siak/index.js
@@ -2,9 +2,13 @@ import Helmet from 'react-helmet'
 import React from 'react'
 import Vue from 'vue'
 
-export default () => {
-  // don't place it outside of the function because window is not defined in webpack (server side)
+// hacky trick to overcome the Helmet lazy refresh problem.
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV == 'development') {
   window.Vue = Vue
+}
+
+export default () => {
   return (
     <div>
       <Helmet
@@ -12,6 +16,7 @@ export default () => {
           { name: 'description', content: 'Susun jadwal SIAK-mu di sini dan pastikan kamulah juaranya.' },
         ]}
         script={[
+          { src: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js' },
           { src: 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.2.0/webcomponents-loader.js' },
           { src: '/jadwal-siak/dist/jadwal-siak.min.js' },
         ]}
