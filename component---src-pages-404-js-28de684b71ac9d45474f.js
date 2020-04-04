a8b24bbac6cbb367572626e91838bae665a07582
M. Fairuzi Teguh <mfairuzit@gmail.com>
2020-04-05 02:07:06 +0700

    Updates

---
+++ component---src-pages-404-js-28de684b71ac9d45474f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(n,a,t){"use strict";t.r(a);t("q1tI");var u=t("Xj0o"),d=t("qKvR");a.default=function(){return Object(d.d)(u.a,{mid:!0},Object(d.d)("h1",null,"404"),Object(d.d)("h5",null,"Halaman yang Anda Tuju Tidak Ditemukan"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-28de684b71ac9d45474f.js.map
