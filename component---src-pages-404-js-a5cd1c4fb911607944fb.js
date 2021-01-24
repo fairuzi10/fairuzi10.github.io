aba3f515e8ee9162b08864885177b3947bb84cde
Muhammad Fairuzi Teguh <mfairuzit@gmail.com>
2021-01-24 11:09:03 +0700

    Updates

---
+++ component---src-pages-404-js-a5cd1c4fb911607944fb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(n,a,t){"use strict";t.r(a);t("q1tI");var u=t("Xj0o"),d=t("qKvR");a.default=function(){return Object(d.d)(u.a,{mid:!0},Object(d.d)("h1",null,"404"),Object(d.d)("h5",null,"Halaman yang Anda Tuju Tidak Ditemukan"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a5cd1c4fb911607944fb.js.map
